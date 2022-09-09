68d4de7c0026cb8867173ff37c5d4c93ff15ccbc
VBasko <106957194+VBasko@users.noreply.github.com>
2022-09-09 06:45:26 +0400

    Added initial styling
    Will be changed later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/vbasko/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Viktoriia Basko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/VBasko/weather-react-simple"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/vbasko/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Viktoriia Basko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/VBasko/weather-react-simple"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
