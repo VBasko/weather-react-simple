3b0f6984c692d66cc780eb779602a819240dc5e4
VBasko <106957194+VBasko@users.noreply.github.com>
2022-09-09 07:49:08 +0400

    Created basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
+import Weather from "./Weather";
+
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <h1>Weather App</h1>
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/vbasko/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Viktoriia Basko
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/VBasko/weather-react-simple"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/vbasko/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Viktoriia Basko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/VBasko/weather-react-simple"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
