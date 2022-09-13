a201655e261b4a7ae7d94cecfec73ce90334ea79
VBasko <106957194+VBasko@users.noreply.github.com>
2022-09-13 07:31:44 +0400

    Created a layout for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,54 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 function WeatherForecast() {
   return (
     <div className="WeatherForecast">
-      <div></div>
+      <div className="forecast-day active">
+        <p>Mon</p>
+        <p className="icon">
+          <WeatherIcon code="01d" />
+        </p>
+        <p>
+          25° <span class="minimum">21°</span>
+        </p>
+      </div>
+      <div className="forecast-day">
+        <p>Mon</p>
+        <p className="icon">
+          <WeatherIcon code="01d" />
+        </p>
+        <p>
+          25° <span class="minimum">21°</span>
+        </p>
+      </div>
+      <div className="forecast-day">
+        <p>Mon</p>
+        <p className="icon">
+          <WeatherIcon code="01d" />
+        </p>
+        <p>
+          25° <span class="minimum">21°</span>
+        </p>
+      </div>
+      <div className="forecast-day">
+        <p>Mon</p>
+        <p className="icon">
+          <WeatherIcon code="01d" />
+        </p>
+        <p>
+          25° <span class="minimum">21°</span>
+        </p>
+      </div>
+      <div className="forecast-day">
+        <p>Mon</p>
+        <p className="icon">
+          <WeatherIcon code="01d" />
+        </p>
+        <p>
+          25° <span class="minimum">21°</span>
+        </p>
+      </div>
     </div>
   );
 }
